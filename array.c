c54c4d9e61e89263cacd87acbd65fa6a053a4232
Nithya Ganesh 43 <nithyaganesh4343@gmail.com>
2024-01-07 21:09:50 +0530

    d

--- array.c
+++ array.c
@@ -1,27 +1,20 @@
 #include <stdio.h>
 int main() {
-    int a[2][2],sum[2]={0,0};
-   //geting
-    printf(" ");
-    for(int i=0;i<2;i++)
-    {
+   int a[10],b[10],c[10];
+   for(int i=0;i<5;i++){
+    printf("enter the %dth element of 1st array ",i+1);
+   scanf("%d",&a[i]);
+   }
+   
+   for(int i=0;i<5;i++){
     
-    for(int j=0;j<2;j++){
-        scanf("%d",&a[i][j]);
-    }
-    }
-    //working
-    for(int i=0;i<2;i++)
-    {
-    for(int j=0;j<2;j++){
-    sum[i]=sum[i]+a[j][i];
-    }
-    }
-    //printing
-    for(int i=0;i<2;i++)
-    {
-    printf(" \n\n ");{
-    printf("%d\t",sum[i]);
-    }
-    }
+    printf("enter the %dth element of 2nd array ",i+1);
+   scanf("%d",&b[i]);
+   }
+   for(int i=0;i<5;i++){
+c[i]=a[i]+b[i];
+   }
+     for(int i=0;i<5;i++)
+   printf("enter the %dth element of array sum is %d\n ",i+1,c[i]);
+    return 0;
 }
